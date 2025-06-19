54c052891c9b8d5e0124b586e704c2c2d8236913
ABenii98 <139451249+ABenii98@users.noreply.github.com>
2025-06-19 17:27:26 -0400

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,6 @@ const awsmobile = {
     region: 'us-east-1',
     userPoolId: 'us-east-1_3fP8HB0Me',
     userPoolWebClientId: '5u4h8am0vq2mideg3iuo429pnp',
-    userPoolArn: 'arn:aws:cognito-idp:us-east-1:673341113430:userpool/us-east-1_3fP8HB0Me',
     authenticationFlowType: 'USER_SRP_AUTH',
   },
 };
